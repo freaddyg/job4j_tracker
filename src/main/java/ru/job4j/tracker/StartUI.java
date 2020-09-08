2a4662e011ed3b515c8ff69fd334d7ebc29465f4
freaddyg <keepzor@mail.ru>
2020-09-08 12:55:47 +0300

    7. Date. Отображение даты. [#345502]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public Item time;
+
+    public StartUI (Item item) {
+        time = item;
+    }
+
+    public static void main(String[] args) {
+        StartUI timeNow = new StartUI(new Item());
+        LocalDateTime currentTime = timeNow.time.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(currentTime.format(formatter));
+    }
+}
