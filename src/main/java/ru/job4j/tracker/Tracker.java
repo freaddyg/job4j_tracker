8cde4f9485f612c4a32c96a78d2ba8d057ed8b96
freaddyg <keepzor@mail.ru>
2020-09-10 19:06:34 +0300

    исправил

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -21,16 +21,7 @@ public class Tracker {
 
 
     public Item[] findAll() {
-        Item[] itemsWithoutNull = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            if (items[i] != null) {
-                itemsWithoutNull[count] = items[i];
-                count++;
-            }
-        }
-        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, count);
-        return itemsWithoutNull;
+        return Arrays.copyOf(items, size);
     }
 
 
