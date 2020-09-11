b02393c2777b75f8e49216a98d18c7a22e71dbc8
freaddyg <keepzor@mail.ru>
2020-09-11 18:00:14 +0300

    2. Scanner и чтение числа из консоли. [#345458]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -4,33 +4,62 @@ import java.util.Scanner;
 
 public class Matches {
 
-    public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-
-        int matches = 11;
+    public void game(int matches, int player, Scanner scanner) {
         while (matches > 0) {
-            System.out.print("Игрок - 1 введите число от 1 до 3: ");
-            int numPlayer1 = Integer.valueOf(inp.nextLine());
+            System.out.print("Игрок - " + player + " введите число от 1 до 3: ");
+            int numPlayer1 = Integer.valueOf(scanner.nextLine());
             if (numPlayer1 < 1 || numPlayer1 > 3) {
                 System.out.println("Неверное число, вы потеряли ход!");
+                if(player == 1) {
+                    player++;
+                } else {
+                    player--;
+                }
             } else {
                 matches -= numPlayer1;
                 if (matches < 0) break;
                 System.out.println("Осталось " + matches + " спичек");
-            }
-
-
-            System.out.print("Игрок - 2 введите число от 1 до 3: ");
-            int numPlayer2 = Integer.valueOf(inp.nextLine());
-            if (numPlayer2 < 1 || numPlayer2 > 3) {
-                System.out.println("Неверное число, вы потеряли ход!");
-            } else {
-                matches -= numPlayer2;
-                if (matches < 0) break;
-                System.out.println("Осталось " + matches + " спичек");
+                if(player == 1) {
+                    player++;
+                } else {
+                    player--;
+                }
             }
         }
-
-        System.out.println("Игра окончена");
     }
+
+
+    public static void main(String[] args) {
+
+        int matches = 11;
+        int player = 1;
+        Scanner scanner = new Scanner(System.in);
+
+        new Matches().game(matches, player, scanner);
+    }
+
+//        while (matches > 0) {
+//            System.out.print("Игрок - 1 введите число от 1 до 3: ");
+//            int numPlayer1 = Integer.valueOf(inp.nextLine());
+//            if (numPlayer1 < 1 || numPlayer1 > 3) {
+//                System.out.println("Неверное число, вы потеряли ход!");
+//            } else {
+//                matches -= numPlayer1;
+//                if (matches < 0) break;
+//                System.out.println("Осталось " + matches + " спичек");
+//            }
+//
+//
+//            System.out.print("Игрок - 2 введите число от 1 до 3: ");
+//            int numPlayer2 = Integer.valueOf(inp.nextLine());
+//            if (numPlayer2 < 1 || numPlayer2 > 3) {
+//                System.out.println("Неверное число, вы потеряли ход!");
+//            } else {
+//                matches -= numPlayer2;
+//                if (matches < 0) break;
+//                System.out.println("Осталось " + matches + " спичек");
+//            }
+//        }
+//
+//        System.out.println("Игра окончена");
 }
