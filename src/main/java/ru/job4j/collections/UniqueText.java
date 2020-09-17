535b6331bf1e5ea2388f7185686de16c83a6cb35
freaddyg <keepzor@mail.ru>
2020-09-17 17:50:20 +0300

    2. Идентичные тексты [#345450]

---
+++ src/main/java/ru/job4j/collections/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collections;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String org : origin) {
+            check.add(org);
+        }
+        for (String txt :text) {
+            if (!check.contains(txt)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
