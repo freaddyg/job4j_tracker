d5565e139a737e94bef74bdf8df427c282ae35ea
freaddyg <keepzor@mail.ru>
2020-09-18 15:12:55 +0300

    0. Map, HashMap [#345444]

---
+++ src/main/java/ru/job4j/collections/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collections;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static HashMap<String, String> data = new HashMap<>();
+
+
+    public static void main(String[] args) {
+        data.put("yuriykorsakov1984@gmail.com", "Korsakov Yuriu");
+        for (String key : data.keySet()) {
+            System.out.println(key + " " + data.get(key));
+        }
+    }
+ }
