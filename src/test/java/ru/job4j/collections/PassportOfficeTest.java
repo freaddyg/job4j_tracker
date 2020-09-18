06b80cffdc77d09a3d49ae32b892c5eb6aaa8a9d
freaddyg <keepzor@mail.ru>
2020-09-18 15:24:34 +0300

    1. Паспорт и жители [#345445]

---
+++ src/test/java/ru/job4j/collections/PassportOfficeTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.collections;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void addTest() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+}
