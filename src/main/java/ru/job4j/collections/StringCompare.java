c99a50b261b8a3f4bfce7d8207ab290915c01837
freaddyg <keepzor@mail.ru>
2020-09-23 20:39:55 +0300

    3. Компаратор для строк [#345430]

--- src/main/java/ru/job4j/collections/StringCompare.java
+++ src/main/java/ru/job4j/collections/StringCompare.java
@@ -5,12 +5,12 @@ import java.util.Comparator;
 import java.util.List;
 
 public class StringCompare implements Comparator<String> {
+
     @Override
     public int compare(String o1, String o2) {
         List<Character> ch1 = new ArrayList<>();
         List<Character> ch2 = new ArrayList<>();
-        int count = 0;
-        int rsl = 0;
+        int rsl;
 
         for (int i = 0; i < o1.length(); i++) {
             ch1.add(o1.charAt(i));
@@ -19,19 +19,15 @@ public class StringCompare implements Comparator<String> {
         for (int i = 0; i < o2.length(); i++) {
             ch2.add(o2.charAt(i));
         }
-
-        while (count < ch1.size() && count < ch2.size()) {
-            rsl = Character.compare(ch1.get(count), ch2.get(count));
+        int length = o1.length() < o2.length() ? o1.length() : o2.length();
+        for (int i = 0; i < length; i++) {
+            rsl = Character.compare(ch1.get(i), ch2.get(i));
             if (rsl < 0 || rsl > 0) {
                 return rsl;
             }
-            count++;
         }
-        if (ch2.size() > ch1.size()) {
-            rsl = -1;
-        } else if (ch2.size() < ch1.size()) {
-            rsl = 1;
-        }
+        rsl = o1.length() - o2.length();
         return rsl;
     }
+
 }
