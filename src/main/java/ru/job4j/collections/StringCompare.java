8cf4bad20125406f422856154178f24cff315036
freaddyg <keepzor@mail.ru>
2020-09-23 20:48:04 +0300

    3. Компаратор для строк [#345430]

--- src/main/java/ru/job4j/collections/StringCompare.java
+++ src/main/java/ru/job4j/collections/StringCompare.java
@@ -1,33 +1,20 @@
 package ru.job4j.collections;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 
 public class StringCompare implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        List<Character> ch1 = new ArrayList<>();
-        List<Character> ch2 = new ArrayList<>();
-        int rsl;
-
-        for (int i = 0; i < o1.length(); i++) {
-            ch1.add(o1.charAt(i));
-        }
-
-        for (int i = 0; i < o2.length(); i++) {
-            ch2.add(o2.charAt(i));
-        }
         int length = o1.length() < o2.length() ? o1.length() : o2.length();
         for (int i = 0; i < length; i++) {
-            rsl = Character.compare(ch1.get(i), ch2.get(i));
-            if (rsl < 0 || rsl > 0) {
-                return rsl;
+            char ch1 = o1.charAt(i);
+            char ch2 = o2.charAt(i);
+            if (ch1 != ch2) {
+                return ch1 - ch2;
             }
         }
-        rsl = o1.length() - o2.length();
-        return rsl;
+        return o1.length() - o2.length();
     }
 
 }
--- src/test/java/ru/job4j/collections/StringCompareTest.java
+++ src/test/java/ru/job4j/collections/StringCompareTest.java
@@ -19,7 +19,7 @@ public class StringCompareTest {
     }
 
     @Test
-    public void whenLeftLessThanRightResultShouldBeNegative () {
+    public void whenLeftLessThanRightResultShouldBeNegative() {
         StringCompare compare = new StringCompare();
         int rst = compare.compare(
                 "Ivanova",
@@ -29,7 +29,7 @@ public class StringCompareTest {
     }
 
     @Test
-    public void whenLeftGreaterThanRightResultShouldBePositive () {
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
         StringCompare compare = new StringCompare();
         int rst = compare.compare(
                 "Petrov",
